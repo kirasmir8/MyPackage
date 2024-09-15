94bc2f89e718665b9650b2a280e86d93b03b3c72
kirasmir88 <kirasmir88@yandex.ru>
2024-09-15 17:42:58 +0300

    my tag

--- myOperation.go
+++ myOperation.go
@@ -1,5 +1,17 @@
 package MyPackage
 
-func Sum() int {
-	return 2 + 5
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
 }
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+} //dsd
